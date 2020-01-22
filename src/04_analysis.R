63909373e2121031eeef069295ecc59f1cc342e9
Daijiang Li <dli1@ufl.edu>
2020-01-22 16:51:11 -0500

    table 1

--- src/04_analysis.R
+++ src/04_analysis.R
@@ -25,14 +25,32 @@ predictors = c("mass_mean",
                "bio15_mean", "bio15_breadth",
                "habitat_axis1", "habitat_axis2", 
                "trophic_axis1", "trophic_axis2")
-
+paste(predictors, collapse = " + ")
 
 summary(lm(ratio_temp_precip_r2 ~ mass_mean + bio1_mean, data = model_results))
 summary(lm(diff_temp_precip_r2 ~ mass_mean + bio1_mean, data = model_results))
 
-mod_1 = lm(diff_temp_precip_r2 ~ mass_mean + bio1_mean + bio1_breadth + bio12_mean + bio12_breadth, 
+mod_1 = lm(diff_temp_precip_r2 ~ mass_mean + bio1_mean + bio1_breadth + bio4_mean + 
+             bio4_breadth + bio12_mean + bio12_breadth + bio15_mean + bio15_breadth + 
+             habitat_axis1 + habitat_axis2 + trophic_axis1 + trophic_axis2, 
            data = filter(model_results, taxa == "birds"))
-summary(mod_1)
+summary(step(mod_1))
+AIC(lm(diff_temp_precip_r2 ~ bio4_mean + bio15_mean, 
+           data = filter(model_results, taxa == "birds")))
+summary(lm(diff_temp_precip_r2 ~ bio4_mean + bio15_mean, 
+           data = filter(model_results, taxa == "birds")))
+# final model
+summary(lm(diff_temp_precip_r2 ~ bio4_mean + bio15_mean + habitat_axis2 + trophic_axis1, 
+       data = filter(model_results, taxa == "birds")))
+
+mod_2 = lm(diff_temp_precip_r2 ~ mass_mean + bio1_mean + bio1_breadth + bio4_mean + 
+             bio4_breadth + bio12_mean + bio12_breadth + bio15_mean + bio15_breadth + 
+             habitat_axis1 + habitat_axis2 + trophic_axis1 + trophic_axis2, 
+           data = filter(model_results, taxa == "mammals"))
+summary(step(mod_2))
+# final model
+summary(lm(formula = diff_temp_precip_r2 ~ bio1_mean + bio15_mean, 
+           data = filter(model_results, taxa == "mammals")))
 
 # elton traits for birds and mammals are generated separately, so they are not comparable.
 
